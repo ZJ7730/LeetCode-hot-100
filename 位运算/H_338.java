4a12fd55368902c058924c54d8b0a455c2283662
zhoujie07 <zhoujie07@zhuanzhuan.com>
2026-06-12 23:52:21 +0800

    feat(*): 25. K 个一组翻转链表、图解思路

--- 位运算/H_338.java
+++ 位运算/H_338.java
@@ -6,4 +6,12 @@
  * @create: 2026-05-26
  **/
 public class H_338 {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+        return dp;
+    }
 }
--- 链表/H_25.java
+++ 链表/H_25.java
@@ -6,4 +6,27 @@
  * @create: 2026-05-28
  **/
 public class H_25 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode nextNode = head;
+        for (int i = 0 ;i <k ;i ++) {
+            if (nextNode == null) {
+                return head;
+            }
+            nextNode = nextNode.next;
+        }
+
+        ListNode cur = head;
+        ListNode pre = null;
+
+        for (int i = 0 ;i <k ;i ++) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+
+        head.next = reverseKGroup(nextNode, k);
+        return pre;
+    }
 }
